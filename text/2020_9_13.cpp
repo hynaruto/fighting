35003c23803af4c4c0874c82872421f3e71eb3cf
huyang <978718289@qq.com>
2020-09-13 17:29:01 +0800

    检测

---
+++ text/2020_9_13.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    a = a+b;
+    b = a-b;
+    a = a-b;
+    printf("%d %d\n",a,b);
+    return 0
+}//
+// Created by 胡杨 on 2020/9/13.
+//
+
+#include "2020_9_13.h"
