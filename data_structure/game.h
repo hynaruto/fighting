4d27c87bd51a42ab5c44fb6b9f3d791bc5b2b75d
huyang <978718289@qq.com>
2020-09-20 21:09:32 +0800

    增加了扫雷简单版小游戏

---
+++ data_structure/game.h
@@ -0,0 +1,20 @@
+//
+// Created by 胡杨 on 2020/9/20.
+//
+
+#ifndef SAOLEI_GAME_GAME_H
+#define SAOLEI_GAME_GAME_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#endif //SAOLEI_GAME_GAME_H
+#define ROW  9
+#define COL  9
+#define ROWS ROW + 2
+#define COLS COL + 2
+#define EASY_COUNT 10
+void  InitBoard(char board[ROWS][COLS],int rows,int cols, char set);
+void DisplayBoard(char board[ROWS][COLS], int row,int col);
+void SetMine(char board[ROWS][COLS],int row,int col);
+void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);
