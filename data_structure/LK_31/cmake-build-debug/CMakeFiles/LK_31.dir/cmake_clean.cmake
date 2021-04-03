34b2b058e590a0dbef36849e40c720fa6c15081b
huyang <978718289@qq.com>
2021-04-03 21:20:33 +0800

    练习

---
+++ data_structure/LK_31/cmake-build-debug/CMakeFiles/LK_31.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LK_31.dir/main.c.o"
+  "LK_31"
+  "LK_31.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LK_31.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ data_structure/LK_31/main.c
@@ -0,0 +1,28 @@
+//https://leetcode-cn.com/leetbook/read/data-structure-binary-tree/xoxzgv/
+
+
+//对称二叉树
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool _isSymmetric(struct TreeNode* t1,struct TreeNode* t2)
+{
+    if (t1 == NULL && t2 == NULL)
+        return true;
+    if (t1 == NULL || t2 == NULL)
+        return false;
+    return (t1->val==t2->val)&&_isSymmetric(t1->left,t2->right)&&_isSymmetric(t1->right,t2->left);
+}
+
+bool isSymmetric(struct TreeNode* root)
+{
+    if (NULL == root)
+        return true;
+    return _isSymmetric(root->left,root->right);
+}
